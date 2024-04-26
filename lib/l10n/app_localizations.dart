fdd353fc109186605e66a42ea5dbaa3ecb910148
SypVuDat <khanhmp9@gmail.com>
2024-04-26 09:14:35 +0700

    submodule

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -16,7 +16,7 @@ import 'app_localizations_vi.dart';
 /// `supportedLocales` list. For example:
 ///
 /// ```dart
-/// import 'localizations/app_localizations.dart';
+/// import 'l10n/app_localizations.dart';
 ///
 /// return MaterialApp(
 ///   localizationsDelegates: AppLocalizations.localizationsDelegates,
